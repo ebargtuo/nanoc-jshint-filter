f3cfb07c1c33ee6d79b882b82334dacbdfb7fd5b
ebargtuo <ebargtuo@gmx.it>
2013-06-21 15:56:30 +0200

    Raise an exception on a fatal js error

--- lib/filters/jshint.rb
+++ lib/filters/jshint.rb
@@ -12,8 +12,11 @@ class JSHintFilter < Nanoc::Filter
     if report != ""
         puts "jshint: #{@item.identifier.chop}.#{@item[:extension]}"
         puts report
+
+        # Raise an exception since a fatal error is probably a show stopper.
+        raise RuntimeError, "jshint: fatal error" if report =~ /fatal error/
     end
-    
+
     # A textual filter should return the content (in our case unchanged).
     content
   end
